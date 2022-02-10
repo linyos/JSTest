1460cdf18a61a457157e8d27035271c06ce293f5
linyos <laso85143@gmail.com>
2022-02-10 23:39:57 +0800

    0210

--- class1.js
+++ class1.js
@@ -6,6 +6,7 @@
 // }
 
 
+
 function myFun(){
     var x = document.getElementById("demotxt").value;
     if(x=="" || isNaN(x))
@@ -13,3 +14,20 @@ function myFun(){
         alert("No Number");
     }
 }
+
+
+
+function fun1(){
+    // var carname = "Volvo";
+    // document.getElementById("p1").innerHTML= carname;
+    var i ;
+    var cars = new Array();
+    cars[0] = "Saab";
+    cars[1] = "Volvo";
+    cars[2] = "BMW";    
+
+    for (i = 0 ; i <cars.length ; i ++) {
+        document.write(cars[i] + "<br>")
+    }
+}
+
