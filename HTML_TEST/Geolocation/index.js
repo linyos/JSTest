44b7ab7e80e9e52febdf821b9880c06ba97e97f9
linyos <laso85143@gmail.com>
2022-02-15 01:04:52 +0800

    location

--- HTML_TEST/Geolocation/index.js
+++ HTML_TEST/Geolocation/index.js
@@ -16,10 +16,27 @@ function getLocation(){
 
 }
 
+
+
 function showPosition(position){
- 
-    x.innerHTML = "緯度: " +position.coords.latitude + "<br>經度: " + position.coords.longitude
-    ;
+    
+    
+    lat=position.coords.latitude;
+    lon = position.coords.longitude;
+    latlon=new google.maps.LatLng(lat, lon);
+    mapholder=document.getElementById('mapholder')
+	mapholder.style.height='250px';
+	mapholder.style.width='500px';
+
+    var myOptions={
+		center:latlon,zoom:14,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		mapTypeControl:false,
+		navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+	};
+	var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+	var marker=new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+
 }
 
 function showError(error){
