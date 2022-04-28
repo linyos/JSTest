fc467db4e1706346ede4a31a5f63e4d7ff602e08
jason <laso85143@gmail.com>
2022-04-28 17:16:01 +0800

    0428

--- JSExample/example/ProgressBar/main.js
+++ JSExample/example/ProgressBar/main.js
@@ -4,7 +4,7 @@ function move(){
   
     //則是固定延遲了某段時間之後，才去執行對應的程式碼，然後「不斷循環」。
     //也會回傳一個獨立的 timer ID
-    var id = setInterval(frame ,500);
+    var id = setInterval(frame ,10);
     function frame(){
         if (width>=100){
             clearInterval(id);
@@ -12,6 +12,7 @@ function move(){
         else{
             width++;
             elem.style.width = width+'%';
+            elem.innerHTML = width+"%";
         }
     }
 
