bfb2568c33746a6259f00331531ca4fbc9454b31
jason <laso85143@gmail.com>
2022-04-28 15:53:57 +0800

    0428

---
+++ JSExample/example/SideBar/main.js
@@ -0,0 +1,15 @@
+function openNav(){
+    //全屏
+   // document.getElementById("mySidenav").style.width= "100%";
+   // 一個區塊
+    document.getElementById("mySidenav").style.display="block"
+}
+
+
+function closeNav(){
+   
+    //document.getElementById("mySidenav").style.width="0%";
+
+    
+    document.getElementById("mySidenav").style.display="nobe";
+}
