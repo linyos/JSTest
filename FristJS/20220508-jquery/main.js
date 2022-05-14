9b088831b8612cfa83cf6f99fca9191f86ad1104
linyos <laso85143@gmail.com>
2022-05-14 17:58:59 +0800

    0514

---
+++ FristJS/20220508-jquery/main.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+    $(".bxslider").bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 600
+        });
+    });
+
+$('h1').css({'color':'red'});
+
+$('#list').html('HTML List');
+
+$('.items').css({'color':'blue'});
